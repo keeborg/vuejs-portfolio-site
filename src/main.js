11f9e15ac2b6a10e3e660f6884a77ed59f427bff
John Keeborg <ibortsov14@gmail.com>
2020-05-10 22:40:54 +0300

    Add parallax

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ if (process.env.NODE_ENV === "development") {
 }
 
 import "./scripts/skills";
-import "./scripts/hamburger_menu";
+import "./scripts/hamburgerMenu";
+import "./scripts/parallax";
---
+++ src/scripts/parallax.js
@@ -0,0 +1,47 @@
+const getWindowWidth = () => {
+    return document.querySelector('body').clientWidth;
+}
+
+window.addEventListener('resize', () => getWindowWidth());
+
+let windowWidth = getWindowWidth();
+
+const parallaxWelcome = document.querySelector('.welcome-sections__parallax');
+
+[parallaxWelcome].forEach((parallax) => {
+    let layers = parallax.querySelectorAll('.parallax__image');
+    
+    function moveLayers(scrollValue) {
+        layers.forEach((layer) => {
+            var layerTransform;
+            
+            if (layer.style.transform !== '') {
+                layerTransform = layer.style.transform;
+            } else {
+                layerTransform = window.getComputedStyle(layer).getPropertyValue('transform');
+            }
+            
+            if (layerTransform !== 'none') {
+                const speedRatio = layer.dataset.speed;
+                let reduction = scrollValue * speedRatio / 10;
+                let translate = layerTransform.match(/translateY\(.+\)/);
+                let newTransform;
+                if (translate !== null) {
+                    newTransform = layerTransform.replace(/translateY\(.+\)/, " translateY(-" + reduction + "%)");
+                } else {
+                    newTransform = layerTransform +" translateY(-" + reduction + "%)";
+                }
+                
+                layer.style.transform = newTransform;
+            }
+        });
+    }
+
+    window.addEventListener('scroll', e => {
+        
+        const scrollValue = window.pageYOffset;
+        moveLayers(scrollValue);
+    })
+});
+
+
