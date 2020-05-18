a4bd566d96ea293d260efd2b31ee116ea42435bb
John Keeborg <ibortsov14@gmail.com>
2020-05-18 22:52:36 +0300

    Finish Works section and create Reviews section on admin page

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -208,12 +208,60 @@
           h1.title Блок "Работы"
         .works-content.container
           .work-form-wrapper
-            .work-form
-          .works-list.card-list
-            .work.card
-              .card-header
+            form.card
+              .card-header Редактирование работы
               .card-content
+                .left
+                  .upload-wrapper
+                    .upload-content
+                      span Перетащите или загрузите для загрузки изображения
+                      button(type="button").gradient-btn.upload-btn Загрузить
+                  //- .preview-wrapper
+                  //-   .work-image-wrapper
+                  //-     ImageCustom(path="content/slider-1.jpg" className="work-image")
+                  //-   button(type="button").change-work-image.text-btn Изменить превью
+                .right
+                  .work-form
+                    .input__wrapper
+                      .input__title Название
+                      .input__block
+                        input(type="text" placeholder="Название работы" required).input__text-type.input.work-title-input
+                    .input__wrapper
+                      .input__title Ссылка
+                      .input__block
+                        input(type="text" placeholder="Ссылка на работу" required).input__text-type.input.work-link-input
+                    .input__wrapper
+                      .input__title Описание
+                      .input__block
+                        textarea(type="textarea" rows="2" placeholder="Описание работы" required).input__textarea-type.input.work-desc-input
+                    .input__wrapper
+                      .input__title Добавление тэга
+                      .input__block
+                        input(type="text" placeholder="Названия тэгов через запятую").input__text-type.input.tag-input
+                    ul.tag-list
+                      li.tag
+                        span HTML
+                        button(type="button").remove-tag
+                          SvgIcon(name="remove" className="remove-tag-icon")
+                      li.tag
+                        span CSS
+                        button(type="button").remove-tag
+                          SvgIcon(name="remove" className="remove-tag-icon")
+                      li.tag
+                        span JavaScript
+                        button(type="button").remove-tag
+                          SvgIcon(name="remove" className="remove-tag-icon")
               .card-footer
+                button(type="button").create-work-cancel-btn.text-btn Отмена
+                button(type="submit").gradient-btn.create-work-btn Сохранить
+          
+          .works-list.card-list
+            
+            button(type="button").add-work.work.card
+              .card-content
+                .circle-add-btn
+                  SvgIcon(name="plus" className="add-btn-icon")
+                | Добавить работу
                 
             .work.card
               .card-header
@@ -278,9 +326,109 @@
                 button(type="button").remove-work
                   span Удалить
                   SvgIcon(name="remove" className="remove-work-icon")
+      .reviews
+        .reviews-header.container
+          h1.title Блок "Отзывы"
+        .reviews-content.container
+          .review-form-wrapper
+            form.card
+              .card-header Новый отзыв
+              .card-content
+                .left
+                  .avatar-wrapper
+                    .review-avatar
+                      SvgIcon(name="user" className="avatar-icon")
+                  button(type="button").text-btn.add-avatar-btn Добавить фото
+                .right
+                  .review-form
+                    .author-inputs
+                      .input__wrapper
+                        .input__title Имя автора
+                        .input__block
+                          input(type="text" placeholder="Введите имя автора" required).input__text-type.input.author-name-input
+                      .input__wrapper
+                        .input__title Должность автора
+                        .input__block
+                          input(type="text" placeholder="Введите должность автора" required).input__text-type.input.author-position-input
+                    .input__wrapper
+                      .input__title Отзыв
+                      .input__block
+                        textarea(type="textarea" rows="2" placeholder="Введите отзыв" required).input__textarea-type.input.review-text-input
+                    
+              .card-footer
+                button(type="button").create-review-cancel-btn.text-btn Отмена
+                button(type="submit").gradient-btn.create-review-btn Сохранить
+          
+          .reviews-list.card-list
+            
+            button(type="button").add-review.review.card
+              .card-content
+                .circle-add-btn
+                  SvgIcon(name="plus" className="add-btn-icon")
+                | Добавить отзыв
+                
+            .review.card
+              .card-header
+                .avatar
+                  ImageCustom(path="content/user.jpg" className="avatar-small-img")
+                .author-name Владимир Сабанцев
+                .author-position Преподаватель
+              .card-content Этот код выдержит любые испытания. Только пожалуйста, не загружайте его на слишком старых браузерах.
+              .card-footer
+                button(type="button").edit-review
+                  span Править
+                  SvgIcon(name="pencil" className="edit-review-icon")
+                button(type="button").remove-review
+                  span Удалить
+                  SvgIcon(name="remove" className="remove-review-icon")
+            
+            .review.card
+              .card-header
+                .avatar
+                  ImageCustom(path="content/user.jpg" className="avatar-small-img")
+                .author-name Владимир Сабанцев
+                .author-position Преподаватель
+              .card-content Этот код выдержит любые испытания. Только пожалуйста, не загружайте его на слишком старых браузерах.
+              .card-footer
+                button(type="button").edit-review
+                  span Править
+                  SvgIcon(name="pencil" className="edit-review-icon")
+                button(type="button").remove-review
+                  span Удалить
+                  SvgIcon(name="remove" className="remove-review-icon")
+            
+            .review.card
+              .card-header
+                .avatar
+                  ImageCustom(path="content/user.jpg" className="avatar-small-img")
+                .author-name Владимир Сабанцев
+                .author-position Преподаватель
+              .card-content Этот код выдержит любые испытания. Только пожалуйста, не загружайте его на слишком старых браузерах.
+              .card-footer
+                button(type="button").edit-review
+                  span Править
+                  SvgIcon(name="pencil" className="edit-review-icon")
+                button(type="button").remove-review
+                  span Удалить
+                  SvgIcon(name="remove" className="remove-review-icon")
+            
+            .review.card
+              .card-header
+                .avatar
+                  ImageCustom(path="content/user.jpg" className="avatar-small-img")
+                .author-name Владимир Сабанцев
+                .author-position Преподаватель
+              .card-content Этот код выдержит любые испытания. Только пожалуйста, не загружайте его на слишком старых браузерах.
+              .card-footer
+                button(type="button").edit-review
+                  span Править
+                  SvgIcon(name="pencil" className="edit-review-icon")
+                button(type="button").remove-review
+                  span Удалить
+                  SvgIcon(name="remove" className="remove-review-icon")
             
             
-            
+           
 </template>
 
 <script>
@@ -315,6 +463,7 @@ export default {
   
   @import "../styles/mixins.pcss";
   @import "../styles/blocks/tag-list.pcss";
+  @import "../styles/blocks/input.pcss";
 
 .main {
   width: 100%;
@@ -358,13 +507,6 @@ export default {
     display: flex;
     align-self: flex-end;
     justify-self: flex-end;
-  }
-  @include tablets {
-    width: 30%;
-  }
-  @include tablets-650 {
-    width: 100%;
-    margin-bottom: 20px;
   }
 }
 
@@ -423,8 +565,7 @@ input {
   }
 }
 
-
-/*===== Circle add button start =====*/
+/*===== Buttons start =====*/
 .add-btn {
   &:hover {
     color: #26288c;
@@ -485,7 +626,29 @@ input {
     }
   }
 }
-/*===== Circle add button end =====*/
+
+.gradient-btn {
+  color: #fff;
+  padding: 15px 45px;
+  text-transform: uppercase;
+  background-image: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+  border-radius: 25px;
+  &:hover {
+    background-image: linear-gradient(to right, #3f35cb 0%, #006aed 100%);
+  }
+  &:active {
+    background-image: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+  }
+}
+
+.text-btn {
+  color: $text-color-active;
+  font-weight: 600;
+  &:hover {
+    color: #000;
+  }
+}
+/*===== Buttons end =====*/
 
 /*===== Header start =====*/
 .header {
@@ -638,6 +801,22 @@ input {
   }
 }
 
+.about-content {
+  .card-content {
+    display: flex;
+    flex-direction: column;
+  }
+  .card {
+    @include tablets {
+      width: 30%;
+    }
+    @include tablets-650 {
+      width: 100%;
+      margin-bottom: 20px;
+    }
+  }
+}
+
 .category-input {
   width: 250px;
   &[readonly="readonly"] {
@@ -687,13 +866,6 @@ input {
 .remove-category,
 .edit-skill-cancel {
   margin-left: 20px;
-}
-
-.about-content {
-  .card-content {
-    display: flex;
-    flex-direction: column;
-  }
 }
 
 .edit-skill,
@@ -759,6 +931,13 @@ input {
 /*===== About end =====*/
 
 /*===== Works start =====*/
+
+.works-content.container {
+  @include tablets-650 {
+    width: 100%;
+    margin: 0;
+  }
+}
 
 .works-header {
   display: flex;
@@ -869,8 +1048,481 @@ input {
     }
   }
   @include tablets {
-    width: 47%;
+    width: 49%;
+    margin-right: 2%;
+    &:nth-child(even) {
+      margin-right: 0;
+    }
+  }
+  @include tabs-phones {
+    width: 100%;
+    min-height: 550px;
+    margin-right: 2%;
+    &:nth-child(even) {
+      margin-right: 0;
+    }
+  }
+}
+
+.add-work {
+  position: relative;
+  background-image: linear-gradient(to right, #006aed 0%, #3f35cb 70%);
+  .card-content {
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 34px;
+    max-width: 195px;
+    display: flex;
+    flex-direction: column;
+    color: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    @include tabs-phones {
+      width: 100%;
+      max-width: 100%;
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+  .circle-add-btn {
+    width: 155px;
+    height: 155px;
+    margin-right: 0;
+    margin-bottom: 30px;
+    background: transparent;
+    border: 2px solid #fff;
+    .add-btn-icon {
+      width: 20%;
+    }
+    @include tabs-phones {
+      width: 70px;
+      height: 70px;
+      margin-bottom: 0;
+      margin-right: 40px;
+    }
+  }
+  @include tabs-phones {
+    width: 100%;
+    height: 150px;
+    min-height: 150px;
+    margin-right: 0;
+  }
+}
+
+.work-form-wrapper {
+  width: 100%;
+  background-color: #fff;
+  .card-content {
+    display: flex;
+    .left {
+      padding: 10px;
+      flex: 1;
+    }
+    .right {
+      padding: 10px;
+      flex: 1;
+    }
+    @include tablets {
+      flex-direction: column;
+      width: 80%;
+      margin: 0 auto;
+    }
+  }
+  .card-footer {
+    @include tablets {
+      width: 100%;
+      justify-content: center;
+    }
+  }
+}
+
+.upload-wrapper {
+  position: relative;
+  width: 100%;
+  min-height: 300px;
+  background-color: #dee4ed;
+  border: 1px dashed #a1a1a1;
+  .upload-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    span {
+      display: block;
+      font-weight: 600;
+      opacity: 0.5;
+      margin-bottom: 20px;
+    }
+  }
+}
+
+.work-image-wrapper {
+  position: relative;
+  width: 100%;
+  height: 350px;
+  overflow: hidden;
+  .work-image {
+    position: absolute;
+    width: 100%;
+    max-height: 900px;
+  }
+}
+
+.preview-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .change-work-image {
+    margin-top: 30px;
+  }
+}
+
+.work-form {
+  margin-bottom: 20px;
+  .tag {
+    padding: 5px 10px 5px 10px;
+    display: flex;
+    span {
+      font-size: 13px;
+      font-weight: 600;
+    }
+    .remove-tag {
+      margin-left: 10px;
+      width: 10px;
+      height: 10px;
+      padding: 0;
+      svg {
+        opacity: 0.5;
+        fill: #000;
+        width: 10px;
+        height: 10px;
+      }
+    }
+  }
+}
+
+.create-work-cancel-btn {
+  margin-right: 40px;
+}
+
+.input__wrapper {
+  margin-bottom: 30px;
+  .input__title {
+    margin-left: 0;
+    margin-bottom: 5px;
+  }
+  .input {
+    font-size: 16px;
+    padding-left: 0;
+    padding-bottom: 10px;
+    font-weight: 600;
+  }
+  .input__textarea-type {
+    margin-top: 10px;
+    border: 1px solid rgba(0,0,0,0.3);
+    padding: 10px;
+  }
+  .input::placeholder {
+    font-size: 16px;
+    opacity: 0.6;
   }
 }
 /*===== Works end =====*/
+
+/*===== Reviews start =====*/
+.reviews-content.container {
+  @include tablets-650 {
+    width: 100%;
+    margin: 0;
+  }
+}
+
+.reviews-header {
+  display: flex;
+  margin-bottom: 60px;
+
+  .title {
+    font-size: 21px;
+    font-weight: 700;
+    margin-right: 60px;
+  }
+  @include tablets-650 {
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 40px;
+    .title {
+      margin-bottom: 30px;
+    }
+  }
+}
+
+.reviews-list {
+  flex-wrap: wrap;
+  justify-content: normal;
+}
+
+.review {
+  margin-right: 10px;
+  width: 30%;
+  margin-right: 3%;
+  min-width: 150px;
+  padding: 0;
+  &:last-child {
+    margin-right: 0;
+  }
+
+  .card-header {
+    display: grid;
+    grid-template-columns: 3;
+    grid-template-rows: 2;
+    padding: 20px;
+    .avatar {
+      width: 45px;
+      height: 45px;
+      grid-column: 1/2;
+      grid-row: 1/3;
+    }
+    .author-name {
+      font-size: 18px;
+      font-weight: 700;
+      grid-column: 2/4;
+      grid-row: 1/2;
+    }
+    .author-position {
+      font-size: 16px;
+      font-weight: 600;
+      opacity: 0.5;
+      grid-column: 2/4;
+      grid-row: 2/3;
+    }
+  }
+
+  .card-content {
+    padding: 10px 20px;
+    font-weight: 600;
+    opacity: 0.7;
+  }
+
+  .card-footer {
+    width: 100%;
+    padding: 20px 20px 30px 20px;
+    .edit-review,
+    .remove-review {
+      display: flex;
+      color: $text-color-light;
+      font-weight: 600;
+      span {
+        opacity: 0.6;
+      }
+      .edit-review-icon {
+        width: 17px;
+        margin-left: 10px;
+        fill: $text-color-active;
+      }
+      .remove-review-icon {
+        width: 17px;
+        margin-left: 10px;
+        fill: #bf2929;
+      }
+    }
+    .edit-review {
+      margin-right: auto;
+      justify-content: flex-start;
+    }
+    .remove-review {
+      margin-left: auto;
+      justify-content: flex-end;
+    }
+    @include desktop-tabs {
+      padding: 30px 10px 30px 10px;
+    }
+  }
+  @include tablets {
+    width: 49%;
+    margin-right: 2%;
+    &:nth-child(even) {
+      margin-right: 0;
+    }
+  }
+  @include tabs-phones {
+    width: 100%;
+    min-height: 250px;
+    margin-right: 2%;
+    &:nth-child(even) {
+      margin-right: 0;
+    }
+  }
+}
+
+.add-review {
+  position: relative;
+  background-image: linear-gradient(to right, #006aed 0%, #3f35cb 70%);
+  .card-content {
+    opacity: 1;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 34px;
+    max-width: 195px;
+    display: flex;
+    flex-direction: column;
+    color: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    @include tabs-phones {
+      width: 100%;
+      max-width: 100%;
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+  .circle-add-btn {
+    width: 155px;
+    height: 155px;
+    margin-right: 0;
+    margin-bottom: 30px;
+    background: transparent;
+    border: 2px solid #fff;
+    .add-btn-icon {
+      width: 20%;
+    }
+    @include tabs-phones {
+      width: 70px;
+      height: 70px;
+      margin-bottom: 0;
+      margin-right: 40px;
+    }
+  }
+  @include tabs-phones {
+    width: 100%;
+    height: 150px;
+    min-height: 150px;
+    margin-right: 0;
+  }
+}
+
+.review-form-wrapper {
+  width: 100%;
+  background-color: #fff;
+  .card-content {
+    display: flex;
+    padding-right: 150px;
+    .left {
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      flex: 1;
+    }
+    .right {
+      padding: 10px;
+      flex: 3;
+      @include tabs-phones {
+        width: 100%;
+      }
+    }
+    @include tablets {
+      padding-right: 50px;
+    }
+    @include tablets-650 {
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+      padding: 0;
+      margin: 0 auto;
+    }
+  }
+  .card-footer {
+    padding-right: 150px;
+    @include tablets {
+      width: 100%;
+      justify-content: center;
+      padding-right: 50px;
+    }
+    @include tablets-650 {
+      padding: 0;
+    }
+  }
+}
+
+.avatar-wrapper {
+  position: relative;
+  width: 250px;
+  height: 250px;
+  background-color: #dee4ed;
+  border-radius: 50%;
+  .avatar-icon {
+    fill: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  @include desktop-tabs {
+    width: 150px;
+    height: 150px;
+  }
+  @include tablets {
+    width: 120px;
+    height: 120px;
+  }
+}
+
+.review-form {
+  margin-bottom: 20px;
+
+}
+
+.create-review-cancel-btn {
+  margin-right: 40px;
+}
+
+.review-form {
+  .input__wrapper {
+    margin-bottom: 30px;
+    .input__title {
+      margin-left: 0;
+      margin-bottom: 5px;
+    }
+    .input {
+      min-width: 250px;
+      font-size: 16px;
+      padding-left: 0;
+      padding-right: 0;
+      padding-bottom: 10px;
+      font-weight: 600;
+      @include tablets {
+        min-width: 220px;
+      }
+    }
+    .input__textarea-type {
+      width: 100%;
+      margin-top: 10px;
+      border: 1px solid rgba(0,0,0,0.3);
+      padding: 10px;
+    }
+    .input::placeholder {
+      font-size: 16px;
+      opacity: 0.6;
+    }
+  }
+  .author-inputs {
+    display: flex;
+    .input__wrapper:nth-child(1) {
+      margin-right: 20px;
+    }
+    @include tabs-phones {
+      flex-direction: column;
+    }
+  }
+}
+
+.add-avatar-btn {
+  margin-top: 20px;
+}
+/*===== Reviews end =====*/
 </style>
